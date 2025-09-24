fb0f6af0951ee68a7f9b6f4fa96721c923ced130
Scypo <androskar7@gmail.com>
2025-09-24 18:18:04 +0200

    Implemented ECS system

---
+++ ScypLib/include/ScypLib/EntityComponentSystem.h
@@ -0,0 +1,368 @@
+#pragma once
+#include<functional>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+#include<queue>
+#include<memory>
+#include<algorithm>
+#include<cassert>
+#include<bitset>
+
+namespace sl
+{
+	constexpr size_t MAX_COMPONENTS = 512;
+	using EntityId = uint32_t;
+	using ArchetypeMask = std::bitset<MAX_COMPONENTS>;
+	using ComponentId = uint32_t;
+	using SystemId = uint32_t;
+
+	class System;
+	class Scene
+	{
+	private:
+		struct ComponentArrayBase
+		{
+			virtual ~ComponentArrayBase() = default;
+			virtual void Resize(size_t newSize) = 0;
+			virtual void Swap(size_t index1, size_t index2) = 0;
+			virtual void PopBack() = 0;
+			virtual void PushBack(void* src) = 0;
+			virtual void Insert(void* src, size_t index) = 0;
+			virtual void Remove(size_t index) = 0;
+			virtual size_t Size() = 0;
+			virtual std::unique_ptr<ComponentArrayBase> CloneEmpty() const = 0;
+			virtual void CopyFrom(size_t destIndex, ComponentArrayBase* src, size_t srcIndex) = 0;
+		};
+		template<typename T>
+		struct ComponentArray : ComponentArrayBase
+		{
+			void Resize(size_t newSize) override;
+			void Swap(size_t index1, size_t index2) override;
+			void PopBack() override;
+			void PushBack(void* src) override;
+			void Insert(void* src, size_t index) override;
+			void Remove(size_t index) override;
+			size_t Size() override;
+			std::unique_ptr<ComponentArrayBase> CloneEmpty() const override;
+			void CopyFrom(size_t destIndex, ComponentArrayBase* src, size_t srcIndex) override;
+			std::vector<T> components;
+		};
+
+		class Archetype
+		{
+		public:
+			Archetype() = default;
+			Archetype(const Archetype& other);
+			template<typename Component>
+			void AddComponent(ComponentId componentId);
+			void RemoveComponent(ComponentId componentId);
+
+			~Archetype() = default;
+			void AddEntity(EntityId entity);
+			void RemoveEntity(EntityId entity);
+			bool Empty();
+		public:
+			std::unordered_map<ComponentId, size_t> componentIdToIndex;
+			std::vector<EntityId> indexToEntityId;
+			std::unordered_map<EntityId, size_t> entityIdToIndex;
+			std::vector<ComponentId> indexToComponentId;
+			std::vector<std::unique_ptr<ComponentArrayBase>> components;
+		};
+	public:
+		Scene() = default;
+		EntityId CreateEntity();
+		template<typename Component>
+		void AddComponent(EntityId entity, Component&& comp);
+		template<typename SystemType>
+		void RegisterSystem();
+		template<typename ...ComponentTypes>
+		void RegisterArchetype();
+
+		template<typename T>
+		ComponentId GetComponentId();
+		template<typename T>
+		SystemId GetSystemId();
+		template<typename ...Types>
+		ArchetypeMask GetArchetypeMask();
+		template<typename Component>
+		Component& GetComponent(EntityId entity);
+		bool IsEntityValid(EntityId entity) const;
+		template<typename Component>
+		bool HasComponent(EntityId entity);
+		void MoveEntity(EntityId entity, const ArchetypeMask& newMask);
+		template<typename ...Components, typename Func>
+		void ForEach(Func&& func);
+
+		void DestroyEntity(EntityId entity);
+		template<typename Component>
+		void RemoveComponent(EntityId entity);
+		template<typename SystemType>
+		void UnregisterSystem();
+		template<typename ...ComponentTypes>
+		void UnregisterArchetype();
+		void Clear();
+	private:
+		SystemId GenerateSystemId();
+		ComponentId GenerateComponentId();
+	public:
+		std::unordered_map<EntityId, ArchetypeMask> entitiesToMask;
+		std::priority_queue<EntityId, std::vector<EntityId>, std::greater<EntityId>> availableId;
+		std::unordered_map<ArchetypeMask, std::unique_ptr<Archetype>> archetypes;
+		std::unordered_set<ArchetypeMask> userCreatedArchetypes;
+		std::unordered_map<SystemId, std::unique_ptr<System>> systems;
+	};
+
+	class System
+	{
+	public:
+		virtual ~System() = default;
+		virtual void Run(float dt, Scene& scene) = 0;
+	};
+
+	class EntityComponentSystem
+	{
+	public:
+		EntityComponentSystem() = default;
+		~EntityComponentSystem() = default;
+
+		Scene* GetCurrentScene();
+		Scene* GetScene(const std::string& sceneName);
+		void SetCurrentScene(const std::string& sceneName);
+		void CreateScene(const std::string& sceneName);
+		void RemoveScene(const std::string& sceneName);
+		void Clear();
+	private:
+		std::unordered_map<std::string, std::unique_ptr<Scene>> scenes;
+		Scene* currentScene = nullptr;
+	};
+
+	template<typename Component>
+	inline void Scene::AddComponent(EntityId entity, Component&& comp)
+	{
+		assert(entitiesToMask.contains(entity));
+		ArchetypeMask currentMask = entitiesToMask[entity];
+
+		if (currentMask.test(GetComponentId<Component>())) return;
+
+		ArchetypeMask newMask = currentMask;
+		newMask.set(GetComponentId<Component>());
+		Archetype* newArchetype = archetypes[newMask].get();
+
+		if (!newArchetype)
+		{
+			if (archetypes[currentMask])
+			{
+				archetypes[newMask] = std::make_unique<Archetype>(*archetypes[currentMask]);
+			}
+			else
+			{
+				archetypes[newMask] = std::make_unique<Archetype>();
+			}
+
+			newArchetype = archetypes[newMask].get();
+			newArchetype->AddComponent<Component>(GetComponentId<Component>());
+		}
+
+		assert(newArchetype);
+		MoveEntity(entity, newMask);
+
+		GetComponent<Component>(entity) = std::forward<Component>(comp);
+	}
+
+	template<typename SystemType>
+	inline void Scene::RegisterSystem()
+	{
+		systems[GetSystemId<SystemType>()] = std::make_unique<SystemType>();
+	}
+
+	template<typename ...ComponentTypes>
+	inline void Scene::RegisterArchetype()
+	{
+		ArchetypeMask mask = GetArchetypeMask<ComponentTypes...>();
+		if (archetypes.contains(mask)) return;
+
+		std::unique_ptr<Archetype> archetype = std::make_unique<Archetype>();
+		((archetype->AddComponent<ComponentTypes>(GetComponentId<ComponentTypes>())), ...);
+		archetypes[mask] = std::move(archetype);
+
+		userCreatedArchetypes.insert(mask);
+	}
+
+	template<typename Component>
+	inline Component& Scene::GetComponent(EntityId entity)
+	{
+		assert(IsEntityValid(entity));
+		assert(HasComponent<Component>(entity));
+		Archetype* arch = archetypes[entitiesToMask[entity]].get();
+		assert(arch);
+		size_t compIdx = arch->componentIdToIndex[GetComponentId<Component>()];
+		assert(static_cast<ComponentArray<Component>*>(arch->components[compIdx].get())->Size() > arch->entityIdToIndex[entity]);
+		return static_cast<ComponentArray<Component>*>(arch->components[compIdx].get())->components[arch->entityIdToIndex[entity]];
+	}
+
+	template<typename Component>
+	inline bool Scene::HasComponent(EntityId entity)
+	{
+		ArchetypeMask mask = entitiesToMask[entity];
+		return mask.test(GetComponentId<Component>());
+	}
+
+	template<typename... Components, typename Func>
+	void Scene::ForEach(Func&& func)
+	{
+		ArchetypeMask desiredMask = GetArchetypeMask<Components...>();
+
+		for (auto& [mask, archPtr] : archetypes)
+		{
+			if ((mask & desiredMask) != desiredMask) continue;
+
+			Archetype* arch = archPtr.get();
+			size_t entityCount = arch->indexToEntityId.size();
+
+			for (size_t i = 0; i < entityCount; i++)
+			{
+				EntityId entity = arch->indexToEntityId[i];
+
+				func(entity, GetComponent<Components>(entity)...);
+			}
+		}
+	}
+
+	template<typename Component>
+	inline void Scene::RemoveComponent(EntityId entity)
+	{
+		assert(IsEntityValid(entity));
+		assert(HasComponent<Component>(entity));
+		ArchetypeMask currentMask = entitiesToMask[entity];
+		ArchetypeMask newMask = currentMask;
+		newMask.reset(GetComponentId<Component>());
+
+		Archetype* newArchetype = archetypes[newMask].get();
+
+		if (!newArchetype)
+		{
+			if (archetypes[currentMask])
+			{
+				archetypes[newMask] = std::make_unique<Archetype>(*archetypes[currentMask]);
+			}
+			else
+			{
+				archetypes[newMask] = std::make_unique<Archetype>();
+			}
+			newArchetype = archetypes[newMask].get();
+			newArchetype->RemoveComponent(GetComponentId<Component>());
+		}
+
+		MoveEntity(entity, newMask);
+	}
+
+	template<typename SystemType>
+	inline void Scene::UnregisterSystem()
+	{
+		systems.erase(GetSystemId<SystemType>());
+	}
+
+	template<typename ...ComponentTypes>
+	inline void Scene::UnregisterArchetype()
+	{
+		ArchetypeMask mask = GetArchetypeMask<ComponentTypes...>();
+		assert(archetypes.contains(mask));
+		archetypes.erase(mask);
+		if (userCreatedArchetypes.contains(mask)) userCreatedArchetypes.erase(mask);
+	}
+
+	template<typename Component>
+	inline void Scene::Archetype::AddComponent(ComponentId componentId)
+	{
+		size_t index = components.size();
+		components.push_back(std::make_unique<ComponentArray<Component>>());
+		componentIdToIndex[componentId] = index;
+		indexToComponentId.push_back(componentId);
+		components[index]->Resize(entityIdToIndex.size());
+	}
+
+	template<typename T>
+	inline ComponentId Scene::GetComponentId()
+	{
+		static ComponentId id = GenerateComponentId();
+		return id;
+	}
+
+	template<typename T>
+	inline SystemId Scene::GetSystemId()
+	{
+		static SystemId id = GenerateSystemId();
+		return id;
+	}
+
+	template<typename ...Types>
+	inline ArchetypeMask Scene::GetArchetypeMask()
+	{
+		ArchetypeMask mask;
+		(mask.set(GetComponentId<Types>()), ...);
+		return mask;
+	}
+
+	template<typename T>
+	inline void Scene::ComponentArray<T>::Resize(size_t newSize)
+	{
+		components.resize(newSize);
+	}
+
+	template<typename T>
+	inline void Scene::ComponentArray<T>::Swap(size_t index1, size_t index2)
+	{
+		assert(index1 < components.size() && index2 < components.size());
+		std::swap(components[index1], components[index2]);
+	}
+
+	template<typename T>
+	inline void Scene::ComponentArray<T>::PopBack()
+	{
+		assert(!components.empty());
+		components.pop_back();
+	}
+
+	template<typename T>
+	inline void Scene::ComponentArray<T>::PushBack(void* src)
+	{
+		assert(src);
+		components.push_back(*static_cast<T*>(src));
+	}
+
+	template<typename T>
+	inline void Scene::ComponentArray<T>::Insert(void* src, size_t index)
+	{
+		assert(src);
+		components[index] = *static_cast<T*>(src);
+	}
+
+	template<typename T>
+	inline void Scene::ComponentArray<T>::Remove(size_t index)
+	{
+		assert(index < components.size());
+		if (index != components.size() - 1) components[index] = components.back();
+		components.pop_back();
+	}
+
+	template<typename T>
+	inline size_t Scene::ComponentArray<T>::Size()
+	{
+		return components.size();
+	}
+
+	template<typename T>
+	inline std::unique_ptr<Scene::ComponentArrayBase> Scene::ComponentArray<T>::CloneEmpty() const
+	{
+		return std::make_unique<ComponentArray<T>>();
+	}
+
+	template<typename T>
+	inline void Scene::ComponentArray<T>::CopyFrom(size_t destIndex, ComponentArrayBase* src, size_t srcIndex)
+	{
+		assert(src);
+		assert(destIndex < components.size() && srcIndex < src->Size());
+		components[destIndex] = static_cast<ComponentArray<T>*>(src)->components[srcIndex];
+	}
+}
---
+++ ScypLib/src/EntityComponentSystem.cpp
@@ -0,0 +1,190 @@
+#include "ScypLib/EntityComponentSystem.h"
+
+namespace sl
+{
+    Scene* EntityComponentSystem::GetCurrentScene()
+    {
+        return currentScene;
+    }
+
+    Scene* EntityComponentSystem::GetScene(const std::string& sceneName)
+    {
+        return scenes[sceneName].get();
+    }
+
+    void EntityComponentSystem::SetCurrentScene(const std::string& sceneName)
+    {
+        assert(scenes.contains(sceneName));
+        currentScene = scenes[sceneName].get();
+    }
+
+    void EntityComponentSystem::CreateScene(const std::string& sceneName)
+    {
+        bool wasEmpty = scenes.empty();
+        scenes[sceneName] = std::make_unique<Scene>();
+        if (wasEmpty) currentScene = scenes[sceneName].get();
+    }
+
+    void EntityComponentSystem::RemoveScene(const std::string& sceneName)
+    {
+        assert(scenes.contains(sceneName));
+        if (currentScene == scenes[sceneName].get()) currentScene = nullptr;
+        scenes.erase(sceneName);
+    }
+
+    void EntityComponentSystem::Clear()
+    {
+        currentScene = nullptr;
+        scenes.clear();
+    }
+
+    EntityId Scene::CreateEntity()
+    {
+        EntityId ent;
+
+        if (!availableId.empty())
+        {
+            ent = availableId.top();
+            availableId.pop();
+        }
+        else
+        {
+            ent = EntityId(entitiesToMask.size());
+        }
+        entitiesToMask[ent].reset();
+        if (!archetypes.contains(0)) archetypes[0] = std::make_unique<Archetype>();
+        archetypes[0].get()->AddEntity(ent);
+        assert(archetypes[0].get()->entityIdToIndex.contains(ent));
+        return ent;
+    }
+
+    bool Scene::IsEntityValid(EntityId entity) const
+    {
+        return entitiesToMask.contains(entity);
+    }
+
+    void Scene::MoveEntity(EntityId entity, const ArchetypeMask& newMask)
+    {
+        if (archetypes[newMask].get() && archetypes[newMask].get()->entityIdToIndex.contains(entity)) return;
+        ArchetypeMask currentMask = entitiesToMask[entity];
+        Archetype* newArchetype = archetypes[newMask].get();
+        assert(newArchetype);
+        assert(currentMask == entitiesToMask[entity]);
+
+        newArchetype->AddEntity(entity);
+        size_t newIndex = newArchetype->entityIdToIndex[entity];
+
+        Archetype* currentArchetype = archetypes[currentMask].get();
+        assert(currentArchetype);
+        if (currentMask != 0)
+        {
+            size_t currentIndex = currentArchetype->entityIdToIndex[entity];
+
+            for (auto& [componentId, idx] : currentArchetype->componentIdToIndex)
+            {
+                if (newArchetype->componentIdToIndex.contains(componentId))
+                {
+                    newArchetype->components[newArchetype->componentIdToIndex[componentId]].get()->CopyFrom(newIndex,
+                        currentArchetype->components[idx].get(), currentIndex);
+                }
+            }
+        }
+        currentArchetype->RemoveEntity(entity);
+        if (currentArchetype->Empty() && !userCreatedArchetypes.contains(currentMask)) archetypes.erase(currentMask);
+        entitiesToMask[entity] = newMask;
+    }
+
+    void Scene::DestroyEntity(EntityId entity)
+    {
+        archetypes[entitiesToMask[entity]].get()->RemoveEntity(entity);
+        entitiesToMask.erase(entity);
+        availableId.push(entity);
+    }
+
+    ComponentId Scene::GenerateComponentId()
+    {
+        static ComponentId id = 0;
+        return id++;
+    }
+
+    void Scene::Clear()
+    {
+        entitiesToMask.clear();
+        while (!availableId.empty()) availableId.pop();
+        archetypes.clear();
+        userCreatedArchetypes.clear();
+        systems.clear();
+    }
+
+    SystemId Scene::GenerateSystemId()
+    {
+        static SystemId id = 0;
+        return id++;
+    }
+
+    Scene::Archetype::Archetype(const Archetype& other)
+    {
+        componentIdToIndex = other.componentIdToIndex;
+        indexToComponentId = other.indexToComponentId;
+        for (auto& comp : other.components)
+        {
+            components.push_back(comp.get()->CloneEmpty());
+        }
+    }
+
+    void Scene::Archetype::RemoveComponent(ComponentId componentId)
+    {
+        size_t index = componentIdToIndex[componentId];
+        size_t lastIndex = componentIdToIndex.size() - 1;
+        if (index != lastIndex)
+        {
+            std::swap(components[index], components[lastIndex]);
+            ComponentId swappedId = indexToComponentId[lastIndex];
+            indexToComponentId[index] = swappedId;
+            componentIdToIndex[swappedId] = index;
+        }
+        components.pop_back();
+        indexToComponentId.pop_back();
+        componentIdToIndex.erase(componentId);
+    }
+
+    void Scene::Archetype::AddEntity(EntityId entity)
+    {
+        size_t index = entityIdToIndex.size();
+        entityIdToIndex[entity] = index;
+        indexToEntityId.push_back(entity);
+
+        for (auto& c : components)
+        {
+            c->Resize(index + 1);
+            assert(c->Size() != 0);
+        }
+    }
+
+    void Scene::Archetype::RemoveEntity(EntityId entity)
+    {
+        assert(entityIdToIndex.contains(entity));
+        size_t index = entityIdToIndex[entity];
+        size_t lastIndex = entityIdToIndex.size() - 1;
+        EntityId lastEntity = indexToEntityId[lastIndex];
+
+        for (auto& comp : components)
+        {
+            comp.get()->Remove(index);
+        }
+
+        if (index != lastIndex)
+        {
+            entityIdToIndex[lastEntity] = index;
+            indexToEntityId[index] = lastEntity;
+        }
+
+        entityIdToIndex.erase(entity);
+        indexToEntityId.pop_back();
+    }
+
+    bool Scene::Archetype::Empty()
+    {
+        return entityIdToIndex.empty();
+    }
+}
